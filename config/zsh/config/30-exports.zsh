288fbcd65b8456863d76d3cfc2444580348ef5d1
Maximilian Kerp <kerp@taktsoft.com>
2026-05-15 23:49:54 +0200

    Consolidate exports

--- config/zsh/config/30-exports.zsh
+++ config/zsh/config/30-exports.zsh
@@ -1,3 +1,6 @@
+# enable colored output from ls, etc. on FreeBSD-based systems
+export CLICOLOR=1
+
 # FZF Base16 Tomorrow Night Theme
 export FZF_DEFAULT_OPTS=' --color=fg:#969896,bg:#1d1f21,hl:#b5bd68 --color=fg+:#e0e0e0,bg+:#1d1f21,hl+:#b5bd68 --color=info:#81a2be,prompt:#cc6666,pointer:#cc6666 --color=marker:#b294bb,spinner:#8abeb7,header:#f0c674'
 
--- config/zsh/config/40-color.zsh
+++
@@ -1,2 +0,0 @@
-# enable colored output from ls, etc. on FreeBSD-based systems
-export CLICOLOR=1
