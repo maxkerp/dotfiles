2098f3907afceb2a2af5b27039c9e3405d612023
Maximilian Kerp <kerp@taktsoft.com>
2026-06-10 16:04:26 +0200

    Add btm cli and use default for editing command line in vim

--- config/zsh/config/50-keybindings.zsh
+++ config/zsh/config/50-keybindings.zsh
@@ -4,12 +4,13 @@ bindkey -e
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey '\C-z\C-e' edit-command-line
+bindkey '\C-x\C-e' edit-command-line
 
-function _append_less () {
-    BUFFER="$BUFFER | $PAGER"
-    zle .accept-line
-}
-
-zle -N _append_less
-bindkey '^[l' _append_less
+# TODO: Find a good keybind
+# function _append_less () {
+#     BUFFER="$BUFFER | $PAGER"
+#     zle .accept-line
+# }
+# 
+# zle -N _append_less
+# bindkey '^[l' _append_less
